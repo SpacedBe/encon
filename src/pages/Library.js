242dd35efbb6c8d86fa4a549615d67f4fb723172
Nena Penel <nenapenel@Nenas-MacBook-Pro.local>
2021-01-18 13:58:00 +0100

    added textBlock component

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Textblock from "../components/Textblock/Textblock";
+import TextBlock from "../components/TextBlock/Textblock";
 
 function Library() {
     return (
@@ -7,7 +7,7 @@ function Library() {
             <h1>Library</h1>
 
             <h2>Text Block</h2>
-            <Textblock greyTitle={"Uitdagingen"}
+            <TextBlock greyTitle={"Uitdagingen"}
                        greyContent={"Greenwashing komt ook voor binnen employer branding. (Potentiële) werkgevers die zich duurzamer voordoen dan ze zijn, worden doorprikt door hun (potentiële) werknemers. Anderzijds kan het lage vertrouwen ook liggen aan gebrekkige communicatie vanuit het bedrijf naar zijn werknemers toe: want die gaan niet altijd proactief de juiste informatie opzoeken."}
                        greenTitle={"Opportuniteiten"}
                        greenContent={"Duurzaamheid is belangrijk voor 83% van de werknemers en 78% van de sollicitanten, maar dan moeten ze wel de juiste informatie gecommuniceerd worden, want ze gaan er niet altijd zelf naar op zoek."}/>
